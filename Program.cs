59e3069ae843cb44e29105b75acb98c69b6a3615
michaelsyah34pg-jpg <michael_syah_34pg@student.smktelkom-mlg.sch.id>
2026-01-12 10:06:19 +0700

    Tambah Validasi nilai

--- Program.cs
+++ Program.cs
@@ -10,3 +10,8 @@ name = Console.ReadLine();
 Console.Write("Input Nilai: ");
 nilai = int.Parse(Console.ReadLine());
 
+if (nilai >= 75)
+    Console.WriteLinE("Selamat, Anda Lulus");
+else
+    Console.WriteLine("Maaf, Anda Tidak Lulus");
+

a5d5cf33b4f3ae56ae8ea0dcf2f2e357223f62ee
michaelsyah34pg-jpg <michael_syah_34pg@student.smktelkom-mlg.sch.id>
2026-01-12 09:58:34 +0700

    tambah input nilai & nama

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿int nilai;
+﻿using System.Runtime.InteropServices.Marshalling;
+
+int nilai;
 string name;
 Console.Write("Aplikasi nilai Siswa: ");
+
+Console.Write("Input Nama: ");
+name = Console.ReadLine();
+
+Console.Write("Input Nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
